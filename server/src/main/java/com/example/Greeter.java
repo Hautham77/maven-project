ba86e3aa39ece9ce5a1ab07cc702d46fb47e5d17
Hautham77 <73292952+Hautham77@users.noreply.github.com>
2021-06-18 11:47:42 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -2,11 +2,11 @@ package com.example;
 
 
 public class Greeter {
-
+/* Commentaire */
   public Greeter() {
 
   }
-
+/* Commentaire */
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
